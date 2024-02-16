bbf4d18adc7369166b4d07fa99882548bc645a99
Crowdbotics <team@crowdbotics.com>
2024-02-16 23:50:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gen-ai-personalized-46435",
+  url: "https://gen-ai-personalized-46435.botics.co",
+  api: "https://gen-ai-personalized-46435.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
